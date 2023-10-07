c25a6e99fe43100fec82a227bf996df1d983b3d7
mharbuz <maciej.harbuz@gmail.com>
2023-10-07 00:40:32 +0000

    sitemap and robots

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@ const path = require('path')
 const autoprefixer = require('autoprefixer')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const miniCssExtractPlugin = require('mini-css-extract-plugin')
+const copyPlugin = require('copy-webpack-plugin')
+const replacePlugin = require('string-replace-loader')
+
 
 module.exports = {
   mode: 'development',
@@ -20,7 +23,12 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({ template: './src/index.html', filename: 'index.html', }),
     new HtmlWebpackPlugin({ template: './src/privacy-policy.html', filename: 'privacy-policy.html', }),
-    new miniCssExtractPlugin()
+    new miniCssExtractPlugin(),
+    new copyPlugin({
+      patterns: [
+        { from: 'src/robots.txt', to: 'robots.txt' },
+        { from: 'src/sitemap.xml', to: 'sitemap.xml' }
+    ]}),
   ],
   module: {
     rules: [
@@ -51,6 +59,14 @@ module.exports = {
             loader: 'sass-loader'
           }
         ]
+      },
+      {
+        test: /sitemap\.xml$/,
+        loader: 'string-replace-loader',
+        options: {
+          search: '__WEBPACK_DATE_REPLACE__',
+          raeplace: '2023-10-10',
+        }
       }
     ]
   }
